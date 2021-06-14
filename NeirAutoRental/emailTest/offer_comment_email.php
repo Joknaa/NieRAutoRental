9567afcbb9b46a46078a9e7e1a70060c17d096e6
nianiama <65319995+nianiama@users.noreply.github.com>
2021-06-14 21:34:47 +0100

    Add files via upload

---
+++ NeirAutoRental/emailTest/offer_comment_email.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact Form</title>
+ 
+</head>
+<body>
+<form>
+      <button type="button" onclick="send_offer_Email()" value="Send An Email">Submit</button>
+    </form>
+    <script src="http://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script type="text/javascript">
+        function send_offer_Email() {
+           
+            
+            
+
+           
+                $.ajax({
+                   url: 'send_offer_Email.php',
+                   method: 'POST',
+                   dataType: 'json',
+                   data: {
+                      
+                      
+                     
+                      
+                   }, success: function (response) {
+                        if (response.status == "success")
+                            alert('Email Has Been Sent!');
+                        else {
+                            alert('Please Try Again!');
+                            console.log(response);
+                        }
+                   }
+                });
+            
+        }
+
+        function isNotEmpty(caller) {
+            if (caller.val() == "") {
+                caller.css('border', '1px solid red');
+                return false;
+            } else
+                caller.css('border', '');
+
+            return true;
+        }
+    </script>
+
+
+
+
+
+
+
+
+
+</body>
+</html>
